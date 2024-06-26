1b9c0b9ef56333bd8abdc20e2da87b1d96c80985
Theroiz <164636377+Theroiz@users.noreply.github.com>
2024-06-26 18:34:57 -0300

    Add files via upload

---
+++ src/WrapperTest.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class WrapperTest {
+    public static void main(String[]args)
+    {
+        Double score = 15.7;
+        Integer scoreInt = 5;
+        ArrayList<Integer> pts = new ArrayList<>();
+        
+        pts.add(10);
+        pts.add(5);
+        System.out.println(pts.get(0));
+    }
+}
